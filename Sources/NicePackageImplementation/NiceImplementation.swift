ccccbf448ef79da5e9630398fa5b255b6b064754
Natan Rolnik <natanrolnik@gmail.com>
2020-10-18 20:27:15 +0300

    Add init to implementation

--- Sources/NicePackageImplementation/NiceImplementation.swift
+++ Sources/NicePackageImplementation/NiceImplementation.swift
@@ -1,6 +1,8 @@
 import NicePackageInterfaces
 
 public class NiceImplementation: NiceProtocol {
+    public init() {}
+
     public func niceFunction() {
         print("Nice function called")
     }
